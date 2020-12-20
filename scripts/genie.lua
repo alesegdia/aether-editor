71c18d7091ac780c9d82a2480f59ff1625f31e82
Alejandro Seguí <alesegdia@gmail.com>
2020-12-20 19:33:04 +0100

    moving imgui to aether

--- scripts/genie.lua
+++ scripts/genie.lua
@@ -4,19 +4,6 @@ dofile(path.join(AETHER_DIR, "scripts/util/util.lua"))
 
 aetherConfig()
 
-function imguiBackend()
-	configuration { "backend-sdl" }
-	files {
-		"../module/imgui/backends/imgui_impl_sdl.h",
-		"../module/imgui/backends/imgui_impl_sdl.cpp",
-	}
-	configuration { "backend-allegro" }
-	files {
-		"../module/imgui/backends/imgui_impl_allegro5.h",
-		"../module/imgui/backends/imgui_impl_allegro5.cpp",
-	}
-end
-
 solution "aether-editor"
 	startproject("aether-editor-game")
 	location "../build/"
@@ -26,35 +13,15 @@ solution "aether-editor"
 	
 	aetherBuild()
 
-	aetherGameLib("aether-editor-game")
-		debugdir ("..")
-		targetdir ("../build")
-		imguiBackend()
-		files {
-			"../src/**.h",
-			"../src/**.cpp",
-			"../module/imgui/*.h",
-			"../module/imgui/*.cpp",
-		}
-		includedirs {
-			"../module/imgui",
-			"../module/imgui/backends/",			
-		}
-
 	aetherProject("aether-editor-game-bin")
 		debugdir ("..")
 		targetdir ("../build")
-		imguiBackend()
 		files {
 			"../src/**.h",
 			"../src/**.cpp",
-			"../module/imgui/*.h",
-			"../module/imgui/*.cpp",
 		}
-
 		includedirs {
-			"../module/imgui",
-			"../module/imgui/backends/",			
+			"../src/",			
 		}
 
 
